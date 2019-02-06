2e35bc655a23ae7224186d9a6dc0e41cbfdd19c6
wangxingang <wangxingang0610@sina.com>
2019-02-06 19:50:39 +0800

    Initial commit

---
+++ src/main/java/com/study/SpringBoot01HelloworldConfigApplication.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HelloworldConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01HelloworldConfigApplication.class, args);
+    }
+
+}
+
